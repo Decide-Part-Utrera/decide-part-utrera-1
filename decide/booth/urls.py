17a3ceafc430d2d7d796ff0641480d51f629bdac
Jose Manuel Cuevas Gallardo <61007947+joscuegal1@users.noreply.github.com>
2022-11-17 17:27:19 +0100

    Revert "feature-votings_implementation-002"

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import BoothView
 
-app_name = 'booth'
+
 urlpatterns = [
     path('<int:voting_id>/', BoothView.as_view()),
-    path('binaryVoting/<int:voting_id>/', BinaryBoothView.as_view())
 ]
