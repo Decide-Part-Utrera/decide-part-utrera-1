cb6e3e1485f0cc89977be26b5e7d40ac3d3787e5
mysInma <inmamayo00@gmail.com>
2022-11-24 19:59:25 +0100

    feature-implements-binary-005

--- decide/voting/admin.py
+++ decide/voting/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.utils import timezone
+from django.db.models.base import Model
 
 from .models import QuestionOption
 from .models import Question
 from .models import Voting
+from .models import BinaryVoting, BinaryQuestion, BinaryQuestionOption
 
 from .filters import StartedFilter
 
@@ -46,5 +48,35 @@ class VotingAdmin(admin.ModelAdmin):
     actions = [ start, stop, tally ]
 
 
+class BinaryQuestionOptionInline(admin.TabularInline):
+    model = BinaryQuestionOption
+
+class BinaryQuestionAdmin(admin.ModelAdmin):
+    inlines = [BinaryQuestionOptionInline]
+
+class BinaryVotingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date')
+    readonly_fields = ('start_date', 'end_date', 'pub_key',
+                       'tally', 'postproc')
+    date_hierarchy = 'start_date'
+    list_filter = (StartedFilter,)
+    search_fields = ('name', )
+
+    actions = [start, stop, tally]
+
+def start_vote(ModelAdmin, request, queryset):
+    for v in queryset.all():
+        v.create_pubkey()
+        v.start_date = timezone.now()
+        v.save()
+
+def stop_vote(ModelAdmin, request, queryset):
+    for v in queryset.all():
+        v.end_date = timezone.now()
+        v.save()
+
+
 admin.site.register(Voting, VotingAdmin)
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(BinaryVoting, BinaryVotingAdmin)
+admin.site.register(BinaryQuestion, BinaryQuestionAdmin)
