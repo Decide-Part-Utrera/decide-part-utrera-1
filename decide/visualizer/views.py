ea87aa69792a8facdb884d37979ccc14ae420151
JoseCarlosMorales <josmorbor3@alum.us.es>
2022-11-28 17:06:59 +0100

    feature/graficasYDescargaDatos-003
    Creacion de las graficas y descarga de datos correspondiente a la votación binaria

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -5,7 +5,7 @@ from django.http import Http404
 from django.views.generic.base import View
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
-from voting.models import Voting
+from voting.models import Voting, BinaryVoting
 
 from base import mods
 
@@ -43,3 +43,33 @@ class Votes_csv(View):
             csv_file.writerow([vote["option"], vote["postproc"], vote["votes"]])
         return res
 
+class VisualizerViewBinary(TemplateView):
+    template_name = 'visualizer/visualizer_binary.html'
+
+    def get_context_data(self,voting_id, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            voting = get_object_or_404(BinaryVoting,pk=voting_id)
+            context['voting'] = json.dumps(BinaryVoting.toJson(voting))
+        except:
+            raise Http404
+
+        return context
+
+class VotesBinary_csv(View):
+    def get(self,request,voting_id,*args,**kwargs):
+        try:
+            voting = get_object_or_404(BinaryVoting,pk=voting_id)
+        except:
+            raise Http404
+
+        res = HttpResponse(content_type="text/csv")
+        res['Content-Disposition'] = 'attachment; filename=' + str(voting.id) + '.csv'
+
+        csv_file = csv.writer(res)
+
+        csv_file.writerow(["Opcion", "Puntuacion", "Votos"])
+
+        for vote in voting.postproc:
+            csv_file.writerow([vote["option"], vote["postproc"], vote["votes"]])
+        return res
