eb4a0b5a3b9fa87b9077eb4789b4d0ff747ba3c9
cargarpas1 <carmengpastrana@gmail.com>
2022-12-03 21:19:13 +0100

    feature-homepage-01

---
+++ decide/homepage/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from . import views
+from django.contrib import admin
+from rest_framework_swagger.views import get_swagger_view
+schema_view = get_swagger_view(title='Decide API')
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('admin/', admin.site.urls),
+    path('doc/', schema_view),
+    path('gateway/', include('gateway.urls')),
+
+]
